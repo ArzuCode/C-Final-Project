0db94da300fa7fdf98fbfcb88fab2259d71f6c8d
Arzu.Code <arzuem@code.edu.az>
2023-07-10 23:12:05 +0300

    Start the Final Project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Market_Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Market management system
+            //Program for managing products in the market
+        }
+    }
+}
